cab1ba0e6d6b52aaded8f3d34c4b8b157a56ffd4
Readme16 <Nikolas.Sandmann@t-online.de>
2016-06-16 13:20:20 +0200

    Update

--- osmp_run.c
+++ osmp_run.c
@@ -8,8 +8,8 @@
  */
 
 #include "osmp.h"
-
-pid_t createChild(char *argv[]);
+#include "osmp_sem.h"
+pid_t createChild(char *argv[], int rank);
 
 int main (int argc, char *argv[]){
     if(argc<3){
@@ -28,7 +28,10 @@ int main (int argc, char *argv[]){
         exit(OSMP_ERROR);
     }
     // shm einrichten
-    int shm_id =shmget(shm_key,OSMP_SHM_SIZE, 0660 | IPC_CREAT);
+    int shm_size = (int)sizeof(shm_header)+
+                    child_process_count*(int)sizeof(shm_child_block)+
+                    OSMP_MAX_SLOTS*(int)sizeof(shm_msg);
+    int shm_id =shmget(shm_key,/*shm_size*/OSMP_SHM_SIZE, 0660 | IPC_CREAT);
     if(shm_id==-1){
         perror("shmget error");
         exit(OSMP_ERROR);
@@ -41,23 +44,47 @@ int main (int argc, char *argv[]){
     }
     
     shm_ptr->size = child_process_count;
-    shm_ptr->free_msg = 0;
-    shm_ptr->closed_msg = OSMP_MAX_SLOTS;
+    shm_ptr->cb_offset = sizeof(shm_header);
+    shm_ptr->msg_offset = (int)sizeof(shm_header) + child_process_count * (int)sizeof(shm_child_block);
+    shm_child_block *shm_cb = (void*)shm_ptr + shm_ptr->cb_offset;
+    shm_msg *shm_ms = (void*)shm_ptr + shm_ptr->msg_offset;
     
+    shm_ptr->free_queue_start = shm_ptr->msg_offset;
+    shm_ptr->free_queue_end = shm_ptr->msg_offset + (OSMP_MAX_SLOTS-1) * (int)sizeof(shm_msg);
     
-    for(i=0;i<OSMP_MAX_SLOTS;i++)
-	shm_ptr->msg_num[i]=i;
+    for(i=0;i<OSMP_MAX_SLOTS;i++){
+        shm_ms[i].next= shm_ptr->msg_offset + (i+1) * (int)sizeof(shm_msg);
+    }
     
-        
+    shm_ms[OSMP_MAX_SLOTS-1].next = -1;
+    
+    /*printf("shm_ptr:%x\n",(void*)shm_ptr);
+    printf("cb_offset:%i\n",shm_ptr->cb_offset);
+    printf("msg_offset:%i\n",shm_ptr->msg_offset);
+    printf("shm_cb:%x\n",(void*)shm_cb);
+    printf("shm_ms:%x\n",(void*)shm_ms);*/
+    
     // einrichten der semaphore
-    int semid = semget(shm_key, child_process_count+1, IPC_CREAT | 0640);
     
-    // starten der Kindprozesse
+    int semid = semget(shm_key, 2*child_process_count+2, IPC_CREAT | 0640);
+    semctl(semid, 0, SETVAL, 1);
+    semctl(semid, 1, SETVAL, OSMP_MAX_SLOTS);
+    for(i = 0; i<child_process_count;i++){
+        semctl(semid, 2*i+2, SETVAL,0);
+        semctl(semid, 2*i+3, SETVAL,OSMP_MAX_MESSAGES_PROC);
+    }
+    osmp_sem_init(shm_key);
+    
+    //starten der Kindprozesse
     for(i = 0; i<child_process_count; i++){
-        shm_ptr->child_block[i].send_msg=0;
-        shm_ptr->child_block[i].rcv_msg=0;
-        child_pid[i]=createChild(&argv[2]);
-        shm_ptr->child_block[i].pid=child_pid[i];
+        shm_cb[i].rcv_queue_start=-1;
+        shm_cb[i].rcv_queue_end=-1;;
+        child_pid[i]=createChild(&argv[2], i);
+        shm_cb[i].pid=child_pid[i];
+    }
+    
+    for(i = 0; i<child_process_count; i++){
+        osmp_sem_signal(i*2+2);
     }
     
     if(shmdt(shm_ptr)==-1){
@@ -72,7 +99,6 @@ int main (int argc, char *argv[]){
 	exit(OSMP_ERROR);
 	}
     }
-    
     // semaphore löschen
     semctl(semid,0,IPC_RMID);
     
@@ -82,17 +108,18 @@ int main (int argc, char *argv[]){
         exit(OSMP_ERROR);
     }
     
-    printf("ende\n");
+    //printf("ende\n");
     return OSMP_SUCCESS;
 }
 
-pid_t createChild(char *childArgv[]){
+pid_t createChild(char *childArgv[], int rank){
     pid_t pid =fork();
     if ( pid == -1 ){
         perror("fork()");
         exit(OSMP_ERROR);
     }
     if ( pid == 0 ){
+        osmp_sem_wait(rank*2+2);
         execvp(childArgv[0], childArgv);
         perror("exec error");
         exit(OSMP_ERROR);
@@ -100,26 +127,3 @@ pid_t createChild(char *childArgv[]){
     return pid;
     
 }
-
-/* int status; 
- * pid_t pid;
- *  pid_t child_pid;
-    //char *env_init[] = { "USER=unknown", "PATH=/tmp", NULL };
-    char *env[] = { "unknown", "tmp", NULL };
-    pid = getpid();
-    for(i = 0; i<5; i++){
-        child_pid = fork();
-        if ( child_pid == -1 ){
-            perror("fork()");
-            exit(OSMP_ERROR);
-        }
-        if ( child_pid == 0 ){
-            printf("\n_____________________________\n");
-            printf("KINDPROZESS\n");
-            //execlp("./echoall", "echoall", "Hallo", "EXECLP", NULL); 
-            //execle("./echoall", "echoall", "Hallo", "EXECLE", NULL, env_init);
-            execvp("./echoall", env);
-            
-            perror("execlp() error");
-            exit(OSMP_ERROR);
-        }*/
